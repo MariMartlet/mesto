da8e598098bc134061a52226d4b3fff8ea055707
MariMartlet <um.dia.vai.chegar.a.aurora@gmail.com>
2022-05-07 00:59:55 +0300

    валидация v.0.1

---
+++ scripts/validate.js
@@ -0,0 +1,73 @@
+const formObject = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__save-button",
+  inactiveButtonClass: "popup__save-button_disabled",
+  inputErrorClass: "popup__input_error",
+  errorClass: "popup__input_error-visible",
+  fields: ".popup__set",
+};
+
+//показываем сообщение об обшибке
+const showInputError = (formElement, inputElement, errorMessage, { inputErrorClass, errorClass, ...rest }) => {
+  const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
+  inputElement.classList.add(inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(errorClass);
+};
+//скрываем сообщение об ошибке
+const hideInputError = (formElement, inputElement, { inputErrorClass, errorClass, ...rest }) => {
+  const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
+  inputElement.classList.remove(inputErrorClass);
+  errorElement.classList.remove(errorClass);
+  errorElement.textContent = "";
+};
+//проверяем валидность поля
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+//переключаем состояние кнопки
+function toggleButtonState(inputList, buttonElement, { inactiveButtonClass, ...rest }) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.disabled = true;
+    buttonElement.classList.add(inactiveButtonClass);
+  } else {
+    buttonElement.disabled = false;
+    buttonElement.classList.remove(inactiveButtonClass);
+  }
+}
+//показываем или прячем ошибку в зависимости от валидности поля
+const checkInputValidity = (formElement, inputElement, { ...rest }) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, rest);
+  } else {
+    hideInputError(formElement, inputElement, rest);
+  }
+};
+const setEventListeners = (formElement, { inputSelector, submitButtonSelector, ...rest }) => {
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+  const buttonElement = formElement.querySelector(submitButtonSelector);
+
+  // чтобы проверить состояние кнопки в самом начале
+  toggleButtonState(inputList, buttonElement, rest);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      checkInputValidity(formElement, inputElement, rest);
+      // чтобы проверять его при изменении любого из полей
+      toggleButtonState(inputList, buttonElement, rest);
+    });
+  });
+};
+
+function enableValidation({ formSelector, fields, ...rest }) {
+  const formList = Array.from(document.querySelectorAll(formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => evt.preventDefault());
+    const fieldsetList = Array.from(formElement.querySelectorAll(fields));
+    fieldsetList.forEach((fieldSet) => setEventListeners(fieldSet, rest));
+  });
+}
+enableValidation(formObject);
