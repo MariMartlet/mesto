7e11f43b43b75cfb560efdb4603a792200ab420e
MariMartlet <88598843+MariMartlet@users.noreply.github.com>
2022-06-21 13:18:23 +0300

    Update Card.js

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,4 +1,4 @@
-import openPopup from "./index.js";
+import default function openPopup from "./index.js";
 
 export const popupImage = document.querySelector("#view-image");
 const popupImageText = popupImage.querySelector(".popup__text");
